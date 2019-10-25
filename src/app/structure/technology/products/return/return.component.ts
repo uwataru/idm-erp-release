ffc11df1a7ec77fc788d8b60264b101a6214bc99
tqxat <990128jo@gmail.com>
2019-10-25 10:18:51 +0900

    품질쪽 빌드에러수정, 로고이미지 수정

--- src/app/structure/technology/products/return/return.component.ts
+++ src/app/structure/technology/products/return/return.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { TypeaheadMatch } from 'ngx-bootstrap/typeahead/typeahead-match.class';
 import {AppGlobals} from '../../../../app.globals';
 import {ActivatedRoute} from '@angular/router';
 import {UtilsService} from '../../../../utils.service';
@@ -25,6 +26,9 @@ export class ReturnComponent implements OnInit {
   messages = this.globals.datatableMessages;
 
   inputForm: FormGroup;
+
+  inputPartners: any[] = this.globals.configs['type4Partners'];
+
 
 
 
@@ -53,5 +57,83 @@ export class ReturnComponent implements OnInit {
   ngOnInit() {
     this.panelTitle = '반품관리'
   }
+  Save() {
+    // 실행권한
+    // if (this.isExecutable == false) {
+    //   alert(this.globals.isNotExecutable);
+    //   return false;
+    // }
+
+    let formData = this.inputForm.value;
+    // if (formData.inventory_classification == 3 && !formData.outs_partner_code) {
+    //   alert('거래처를 선택해주세요!');
+    //   return false;
+    // }
+
+    // formData.inventory_classification = formData.inventory_classification * 1;
+    // formData.inspection_date = this.datePipe.transform(formData.inspection_date, 'yyyy-MM-dd');
+    // formData.input_date = this.datePipe.transform(formData.input_date, 'yyyy-MM-dd');
+
+    this.Create(formData);
+  }
+
+  Reset() {
+    this.inputForm.reset();
+    // this.inputForm.controls['input_date'].setValue(this.tDate);
+  }
+
+  Create(data): void {
+    // this.dataService.Create(data)
+    //   .subscribe(
+    //     data => {
+    //       if (data['result'] == 'success') {
+    //         this.Reset();
+    //         //this.getAll();
+    //         this.messageService.add(this.addOkMsg);
+    //       } else {
+    //         this.messageService.add(data['errorMessage']);
+    //       }
+    //     },
+    //     error => this.errorMessage = <any>error
+    //   );
+  }
+
+  onSelectInputPartner(event: TypeaheadMatch): void {
+    if (event.item == '') {
+        this.inputForm.controls['outs_partner_code'].setValue(0);
+    } else {
+        this.inputForm.controls['outs_partner_code'].setValue(event.item.Code);
+    }
+}
+
+  // loadInfo(event) {
+  //   let OrderNo = event.target.value;
+  //   if (!OrderNo) {
+  //     return false;
+  //   }
+
+  //   // 내용
+  //   this.dataService.GetById(OrderNo).subscribe(
+  //     editData => {
+  //       if (editData['result'] == 'success') {
+  //         this.editData = editData;
+  //         this.formData = editData['data'];
+
+  //         this.screeningQty = editData['screeningQty'] * 1;
+  //         console.log(this.formData.normal_qty);
+  //         this.inputForm.patchValue({
+  //           order_no: OrderNo,
+  //           production_date: this.formData.input_date,
+  //           product_name: this.formData.product_name,
+  //           product_code: this.formData.product_code,
+  //           production_qty: this.formData.screening_qty,
+  //           normal_qty: this.formData.normal_qty,
+  //           defective_qty: this.formData.defective_qty,
+  //           defective_classification: this.formData.defective_classification
+  //         });
+  //       }
+  //     }
+  //   );
+  // }
 
 }
--- src/app/structure/technology/products/shipping-packaging/shipping-packaging.component.ts
+++ src/app/structure/technology/products/shipping-packaging/shipping-packaging.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { TypeaheadMatch } from 'ngx-bootstrap/typeahead/typeahead-match.class';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AppGlobals} from '../../../../app.globals';
 import {ActivatedRoute} from '@angular/router';
@@ -25,6 +26,9 @@ export class ShippingPackagingComponent implements OnInit {
 
   inputForm: FormGroup;
 
+  inputPartners: any[] = this.globals.configs['type4Partners'];
+
+
   constructor(
     @Inject(FormBuilder) fb: FormBuilder,
     private globals: AppGlobals,
@@ -50,5 +54,84 @@ export class ShippingPackagingComponent implements OnInit {
   ngOnInit() {
     this.panelTitle = '출하/포장관리';
   }
+
+  Save() {
+    // 실행권한
+    // if (this.isExecutable == false) {
+    //   alert(this.globals.isNotExecutable);
+    //   return false;
+    // }
+
+    let formData = this.inputForm.value;
+    // if (formData.inventory_classification == 3 && !formData.outs_partner_code) {
+    //   alert('거래처를 선택해주세요!');
+    //   return false;
+    // }
+
+    // formData.inventory_classification = formData.inventory_classification * 1;
+    // formData.inspection_date = this.datePipe.transform(formData.inspection_date, 'yyyy-MM-dd');
+    // formData.input_date = this.datePipe.transform(formData.input_date, 'yyyy-MM-dd');
+
+    this.Create(formData);
+  }
+
+  Reset() {
+    this.inputForm.reset();
+    // this.inputForm.controls['input_date'].setValue(this.tDate);
+  }
+
+  Create(data): void {
+    // this.dataService.Create(data)
+    //   .subscribe(
+    //     data => {
+    //       if (data['result'] == 'success') {
+    //         this.Reset();
+    //         //this.getAll();
+    //         this.messageService.add(this.addOkMsg);
+    //       } else {
+    //         this.messageService.add(data['errorMessage']);
+    //       }
+    //     },
+    //     error => this.errorMessage = <any>error
+    //   );
+  }
+
+  onSelectInputPartner(event: TypeaheadMatch): void {
+    if (event.item == '') {
+        this.inputForm.controls['outs_partner_code'].setValue(0);
+    } else {
+        this.inputForm.controls['outs_partner_code'].setValue(event.item.Code);
+    }
+}
+
+  // loadInfo(event) {
+  //   let OrderNo = event.target.value;
+  //   if (!OrderNo) {
+  //     return false;
+  //   }
+
+  //   // 내용
+  //   this.dataService.GetById(OrderNo).subscribe(
+  //     editData => {
+  //       if (editData['result'] == 'success') {
+  //         this.editData = editData;
+  //         this.formData = editData['data'];
+
+  //         this.screeningQty = editData['screeningQty'] * 1;
+  //         console.log(this.formData.normal_qty);
+  //         this.inputForm.patchValue({
+  //           order_no: OrderNo,
+  //           production_date: this.formData.input_date,
+  //           product_name: this.formData.product_name,
+  //           product_code: this.formData.product_code,
+  //           production_qty: this.formData.screening_qty,
+  //           normal_qty: this.formData.normal_qty,
+  //           defective_qty: this.formData.defective_qty,
+  //           defective_classification: this.formData.defective_classification
+  //         });
+  //       }
+  //     }
+  //   );
+  // }
 
 }
