9b39cac18cb06e2c2daa4c21063ab472a0e06576
ironynet <ironynet@gmail.com>
2019-12-17 15:14:37 +0900

    영업>반품명세서/납품불량명세서: 엑셀 작업

--- src/app/structure/sales/order/inferior-goods/inferior-goods.component.ts
+++ src/app/structure/sales/order/inferior-goods/inferior-goods.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
-import { ElectronService } from '../../../../providers/electron.service';
+import {ElectronService, EXPORT_EXCEL_MODE} from '../../../../providers/electron.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TypeaheadMatch } from 'ngx-bootstrap/typeahead/typeahead-match.class';
 import { DatePipe } from '@angular/common';
@@ -8,6 +8,8 @@ import { AppGlobals } from '../../../../app.globals';
 import { UtilsService } from '../../../../utils.service';
 import { MessageService } from '../../../../message.service';
 import { Item } from './inferior-goods.item';
+import {saveAs as importedSaveAs} from "file-saver";
+import {Alignment, Border, Borders, Fill, Font, Workbook} from "exceljs";
 
 @Component({
   selector: 'app-page',
@@ -52,7 +54,7 @@ export class InferiorGoodsComponent implements OnInit {
         @Inject(FormBuilder) fb: FormBuilder,
         private datePipe: DatePipe,
         private dataService: InferiorGoodsService,
-        public electronService: ElectronService,
+        public elSrv: ElectronService,
         private globals: AppGlobals,
         private utils: UtilsService,
         private messageService: MessageService
@@ -103,5 +105,70 @@ export class InferiorGoodsComponent implements OnInit {
         this.searchForm.controls['sch_partner_name'].setValue(event.item['name']);
         this.getAll();
     }
+
+    exportExcel(type: EXPORT_EXCEL_MODE, fileName: string = '') {
+        if (this.elSrv.checkExportExcel()) {
+            let data;
+            if (type == EXPORT_EXCEL_MODE.MASTER) { //마스터파일은 서버에서 자료가져와 생성
+                // data = this.dataService.GetMasterExcelData()['data'];
+            } else { //리스트는 기존 가져온 데이터로 생성
+                data = this.rows;
+            }
+
+            let workbook = new Workbook();
+            let worksheet = workbook.addWorksheet(this.panelTitle);
+
+            worksheet.getColumn(1).width = 12;
+            worksheet.getColumn(2).width = 25;
+            worksheet.getColumn(3).width = 25;
+            worksheet.getColumn(4).width = 15;
+            worksheet.getColumn(5).width = 8;
+            worksheet.getColumn(6).width = 8;
+            worksheet.getColumn(7).width = 10;
+            worksheet.getColumn(8).width = 12;
+
+            const header = ["납품일자", "거래처", "제품명", "수주번호", "납품수량", "불량수량", "단가", "금액"];
+            let headerRow = worksheet.addRow(header);
+            headerRow.font = this.globals.headerFontStyle as Font;
+            headerRow.eachCell((cell, number) => {
+                cell.fill = this.globals.headerFillColor as Fill;
+                cell.border = this.globals.headerBorderStyle as Borders;
+                cell.alignment = this.globals.headerAlignment as Alignment;
+            });
+
+            let jsonValueToArray;
+            data.forEach(d => {
+                  jsonValueToArray = [];
+                  jsonValueToArray.push(d.input_date);
+                  jsonValueToArray.push(d.partner_name);
+                  jsonValueToArray.push(d.product_name);
+                  jsonValueToArray.push(d.order_no);
+                  jsonValueToArray.push(d.qty);
+                  jsonValueToArray.push(d.return_qty);
+                  jsonValueToArray.push(d.product_price);
+                  jsonValueToArray.push(d.sales_price);
+
+                  let row = worksheet.addRow(jsonValueToArray);
+                  row.font = this.globals.bodyFontStyle as Font;
+                  row.getCell(1).alignment = {horizontal: "center"};
+                  row.getCell(4).alignment = {horizontal: "center"};
+                  row.getCell(5).alignment = {horizontal: "right"};
+                  row.getCell(6).alignment = {horizontal: "right"};
+                  row.getCell(7).alignment = {horizontal: "right"};
+                  row.getCell(8).alignment = {horizontal: "right"};
+                  row.eachCell((cell, number) => {
+                      cell.border = this.globals.bodyBorderStyle as Borders;
+                  });
+              }
+            );
+
+            workbook.xlsx.writeBuffer().then((data) => {
+                let blob = new Blob([data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+                fileName = fileName == '' ? this.panelTitle : fileName;
+                importedSaveAs(blob, fileName + '.xlsx');
+            })
+        }
+    }
+
 
 }
