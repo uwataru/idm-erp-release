66fdfda6bf1d7b2dc421eb81289441bbd289b6c3
ironynet <ironynet@gmail.com>
2019-12-17 10:42:23 +0900

    생산>LOT추적표: 1차 작업완료

--- src/app/structure/production/record/lot-tracker/lot-tracker.item.ts
+++ src/app/structure/production/record/lot-tracker/lot-tracker.item.ts
@@ -1,4 +1,22 @@
 export class Item {
     //id: number;
-    name: string;
+    totalCount: number;
+    data: {
+        id: number;
+        order_no: string;
+        delivery_no: number;
+        product_code: string;
+        product_name: string;
+        partner_code: number;
+        partner_name: string;
+        poc_no: string;
+        sales_qty: number;
+        product_price: number;
+        sales_price: number;
+        sales_date: string;
+        delivery_date: string;
+    };
+    maxResultCount: number;
+    result: string;
+    errorMessage: string;
 }
