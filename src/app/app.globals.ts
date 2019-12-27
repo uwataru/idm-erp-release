466a5d488da4362f3792208d917e7ad604db8454
tqxat <990128jo@gmail.com>
2019-12-27 17:35:07 +0900

    실서버 주소 변경

--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -6,7 +6,7 @@ import { ServerConfig } from '../environments/serverinfo';
 export class AppGlobals {
     constructor() { }
 
-    public remoteUrl: string = 'http://lucas.innest.co.kr';
+    public remoteUrl: string = 'http://idm.innest.co.kr';
     public serverUrl: string = this.getServerUrl();
 
 
@@ -45,8 +45,8 @@ export class AppGlobals {
       if(!AppConfig.production){
         this.serverUrl = ServerConfig.myDevServer;
       }else{
-        // this.serverUrl='http://idm.innest.co.kr';
-        this.serverUrl='http://lucas.innest.co.kr';
+        this.serverUrl='http://idm.innest.co.kr';
+        // this.serverUrl='http://lucas.innest.co.kr';
       }
       return this.serverUrl;
     }
