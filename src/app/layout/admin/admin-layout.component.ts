9af7c9988b067abca255dc88829870e24e5f5858
tqxat <990128jo@gmail.com>
2020-04-09 11:35:44 +0900

    시작화면 생산으로 변경

--- src/app/layout/admin/admin-layout.component.ts
+++ src/app/layout/admin/admin-layout.component.ts
@@ -203,7 +203,7 @@ export class AdminLayoutComponent implements OnInit {
             this.titleService.setTitle(event['title']);
             this.page_title = event['title'];
         });
-        this.goToLink(5,'/production',false);
+        this.goToLink(501,'/production/planning/production-line-planning',false);
     }
 
     getActiveGroupMenu() {
